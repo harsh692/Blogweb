75000517b7f0c3cba348104078fa046dd30248b1
Harsh <hg13834@gmail.com>
2022-08-03 20:04:41 +0530

    second commit : created views in core and error pages handler file

--- blog_main/__init__.py
+++ blog_main/__init__.py
@@ -3,3 +3,9 @@
 from flask import Flask
 
 app=Flask(__name__)
+
+from blog_main.core.views import core
+app.register_blueprint(core)
+
+from blog_main.error_pages.handlers import error_pages
+app.register_blueprint(error_pages)
